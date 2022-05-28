9efa9c97981b315abba882b52b36d0f375a57a33
Saiteja <saiteja1304.st@gmail.com>
2022-05-28 20:01:28 +0530

    error viewport

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -3,7 +3,7 @@
 int Graphics::GInit(){
     return gladLoadGL();
 }
-void Graphics::GSetViewPort(int &xPos, int &yPos, int &width, int &height){
+void Graphics::GSetViewPort(int xPos, int yPos, int width, int height){
     glViewport(xPos, yPos, width, height);
 }
 void Graphics::GBackGroundColor(float red, float green, float blue, float alpha){
