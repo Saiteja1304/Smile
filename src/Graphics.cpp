cbbc69dcf60b28276cdeba0be9203a12a1f08fc2
Saiteja <saiteja1304.st@gmail.com>
2022-05-28 20:07:55 +0530

    added glclear to GBackGroundColor fun

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -8,4 +8,5 @@ void Graphics::GSetViewPort(int xPos, int yPos, int width, int height){
 }
 void Graphics::GBackGroundColor(float red, float green, float blue, float alpha){
     glClearColor(red, green, blue, alpha);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
