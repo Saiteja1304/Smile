7d7a95f62caa677d40c19c1f398591277b31cb4f
Saiteja <saiteja1304.st@gmail.com>
2022-06-02 17:11:31 +0530

    added some printing lines to virtula functions

--- include/Scripting/CScript.hpp
+++ include/Scripting/CScript.hpp
@@ -1,13 +1,14 @@
 #ifndef CSCRIPT_HPP
 #define CSCRIPT_HPP
 
+#include <iostream>
 extern "C" class CScript {
 public:
   void nonvirtual();
-  virtual void Init() {}
-  virtual void Start() {}
-  virtual void Update() {}
-  virtual void Exit() {}
+  virtual void Init() {std::cerr << "Init\n";}
+  virtual void Start() {std::cerr << "Start\n";}
+  virtual void Update() {std::cerr << "Update\n";}
+  virtual void Exit() {std::cerr << "Exit\n";}
 
   CScript() {}
   virtual ~CScript() {}
