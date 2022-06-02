3919fea80d0aef89751ba32c10f43274ff001cba
Saiteja <saiteja1304.st@gmail.com>
2022-06-02 19:36:49 +0530

    creates Math Lib added vector4

--- addlibtosystem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo cp -a include/. /usr/local/include/Smile
-sudo cp build/libSmile.so /usr/local/lib/
-set LD_LIBRARY_PATH /usr/local/lib/
-export LD_LIBRARY_PATH
---
+++ include/Core/Math/vector4.hpp
@@ -0,0 +1,11 @@
+#ifndef VECTOR4_HPP
+#define VECTOR4_HPP
+
+struct vector4 {
+  enum { float r, x; };
+  enum { float g, y; };
+  enum { float b, z; };
+  enum { float a, w; };
+};
+
+#endif
