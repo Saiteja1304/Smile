dca6d5ac4399a94f7202c343341a82ff3f5ada73
Saiteja <saiteja1304.st@gmail.com>
2022-06-02 15:59:16 +0530

    edited scripting base class

--- include/Scripting/CScript.hpp
+++ include/Scripting/CScript.hpp
@@ -1,11 +1,6 @@
 #ifndef CSCRIPT_HPP
 #define CSCRIPT_HPP
 
-extern "C" class CScript {
-public:
-  virtual void Start() {}
-  virtual void Update() {}
-  virtual void End() {}
-};
+class CScript {};
 
 #endif
